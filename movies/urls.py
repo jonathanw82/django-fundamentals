46c5571b435d27a64e6bb5e4097ccd59658ce73c
jonathanw82 <jonwhewaycode@outlook.com>
2020-04-27 17:14:35 +0000

    updatet

--- movies/urls.py
+++ movies/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import intro, first_movie, get_all_movies
+from .views import intro, first_movie, get_all_movies, create, read
 
 urlpatterns = [
     path('intro/', intro),
     path('first_movie', first_movie),
     path('all-movies', get_all_movies),
+    path('create', create),
+    path('read', read)
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Movie
 
 
 def intro(request):
@@ -13,26 +14,36 @@ def first_movie(request):
 
 
 def get_all_movies(request):
-    data = {'movies': [{'title': '1- The Good, The Bad The Ugly',
+    data = {'movies': [{'title': 'The Good, The Bad The Ugly',
                         'synopsys': 'A western movie',
-                        'release': '1966'},
+                        'release_year': '1966',
+                        'duration': '120'},
 
-                       {'title': '2- The Good, The Bad The Ugly',
-                        'synopsys': 'A western movie',
-                        'release': '1966'},
+                       {'title': 'Star Wars',
+                        'synopsys': 'Family Fude in space',
+                        'release_year': '1975',
+                        'duration': '185'},
 
 
-                       {'title': '3- The Good, The Bad The Ugly',
-                        'synopsys': 'A western movie',
-                        'release': '1966'}, ]
+                       {'title': 'Star Trek',
+                        'synopsys': 'Blue Blooded alians in space',
+                        'release_year': '1965',
+                        'duration': '120'}, ]
             }
     return render(request, 'movies/all-movies.html', data)
 
+    def create(request):
 
-    def get_all_movies(request):
-
-        movies = Movies.objects.all()
+        movies = Movie(title='my Movie', synopsys='Something There',
+                       release_year='1989', duration=180)
+        movies.save()
 
         data = {'movies': movies}
+        return render(request, 'movies/create.html', data)
 
-        return render(request, 'movies/all-movies.html', data)
+    def read(request):
+
+        movies = Movie.objects.all()
+        data = {'movies': movies}
+
+        return render(request, 'movies/read.html', data)
